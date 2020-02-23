e99a77d3be8fad7d89ecb58fb90bee83fee9921a
Iyngaran <iyngaran@Iyngarans-MacBook-Pro.local>
2020-02-23 07:43:54 +0530

    The migrations for tag

--- database/migrations/2020_02_23_015925_create_tags_table.php
+++ database/migrations/2020_02_23_015925_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTablex extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateTagsTablex extends Migration
     {
         Schema::create('tbl_tags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name',45)->nullable();
+            $table->string('tag',45)->nullable();
             $table->timestamps();
         });
     }
