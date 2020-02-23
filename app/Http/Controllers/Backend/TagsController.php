7dbab8351b4dcecfec6b4072d2d1348899755187
Iyngaran <iyngaran@Iyngarans-MacBook-Pro.local>
2020-02-23 12:00:28 +0530

    The controller for the tags

---
+++ app/Http/Controllers/Backend/TagsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TagsController extends Controller
+{
+    public function create()
+    {
+        return 'Tags - create';
+    }
+
+    public function store()
+    {
+        return 'Tags - store';
+    }
+}
